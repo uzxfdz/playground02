d00e0fdfbac316091259276bc0fe71580a953549
uzxfdz <uzxfdz_01@icloud.com>
2016-08-21 10:31:28 -0500

    playground
    tarea02

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: numeros
+
+import UIKit
+
+for num in 0...100 {
+    if num % 5 == 0 {
+        print ("\(num) Bingo!!!")
+    }
+    if num % 2 == 0 {
+        print ("\(num) par!!!")
+    }else {
+        print ("\(num) impar!!!")
+    }
+    if num >= 30 && num <= 40 {
+        print ("\(num) Viva Swift!!!")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
